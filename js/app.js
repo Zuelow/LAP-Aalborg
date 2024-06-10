1cfc0c1307b79ee4e13512779c3d4e8f995ef9ce
Kasper Zülow <kasperzulow@gmail.com>
2024-06-10 19:44:12 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,181 @@
+/* Theme Name: Mobirus - Responsive Landing Page Template
+   Author: Themesdesign
+   Version: 1.0.0
+   Created: December 2012
+   File Description: Main CSS file of the template
+*/
+
+! function($) {
+    "use strict";
+
+    var PogoApp = function() {};
+
+    PogoApp.prototype.initStickyMenu = function() {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 50) {
+                $(".sticky").addClass("nav-sticky");
+            } else {
+                $(".sticky").removeClass("nav-sticky");
+            }
+        });
+    },
+    
+    PogoApp.prototype.initSmoothLink = function() {
+        $('.menu-nav a').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    },
+
+    PogoApp.prototype.initMenuOpen = function() {
+        $(".menu-btn-open").on("click", function(e) {
+            e.preventDefault();
+            
+            $(".menu-lightbox").fadeIn("normal", function(){$(this).addClass("active")});
+            $(".menu-btn-close").addClass("loaded");
+        });
+    },    
+
+    PogoApp.prototype.initMenuClose = function() {
+        $(".menu-btn-close").on("click", function(e) {
+            e.preventDefault();
+            
+            $(".menu-lightbox").delay(100).removeClass("active").delay(200).fadeOut("slow");
+            $(".menu-btn-close").removeClass("loaded");
+        });
+    }, 
+
+    PogoApp.prototype.initMenuToggleClose = function() {
+        $(".menu-nav li a").on("click", function(e) {
+            $(".menu-btn-close").trigger("click");
+        });
+
+    }, 
+
+PogoApp.prototype.initFullPages = function() {
+    $('#fullpage').fullpage({
+        anchors: ['1stPage', '2ndPage', '3rdPage', '4thPage', '5thPage', '6thPage', '7thPage', '8thPage', '9thPage', '10thPage'],
+        menu: '#menu',
+        scrollingSpeed: 800,
+        autoScrolling: true,
+        scrollBar: true,
+        easing: 'easeInQuart',
+        resize: false,
+        paddingTop: '0px',
+        paddingBottom: '80px',
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Hjem', 'Om LAP', 'Organisation', 'Det mener LAP', 'Billeder', 'Bliver medlem', 'FAQ/OSS', 'Kontakt os'],
+        responsiveWidth: 1100,
+        showActiveTooltip: true,
+        css3: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollOverflow: true,
+        scrollOverflowOptions: {
+            scrollbars: true,
+            mouseWheel: true,
+            hideScrollbars: false,
+            fadeScrollbars: false,
+            disableMouse: true
+        },
+        onLeave: function(index, nextIndex, direction) {
+            // Add your custom code here
+        },
+        afterLoad: function(anchorLink, index) {
+            // Add your custom code here
+        }
+    });
+};
+
+
+    PogoApp.prototype.initVideo = function() {
+        $('.video-play-icon').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    },
+
+    PogoApp.prototype.initScreenshot = function() {
+        $(".owl-screenshot").owlCarousel({
+            items:4,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:1000,
+            autoplayHoverPause:true,
+            itemsDesktop : [1199,1],
+            itemsDesktopSmall : [979,1]
+        });
+    },    
+
+    PogoApp.prototype.initTestimonial = function() {
+        $("#owl-demo").owlCarousel({
+            autoPlay: 3000,
+            stopOnHover: true,
+            navigation: false,
+            paginationSpeed: 1000,
+            goToFirstSpeed: 2000,
+            singleItem: true,
+            autoHeight: true
+        });
+    }, 
+
+    PogoApp.prototype.initScreenMFP = function() {
+        $('.mfp-image').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            mainClass: 'mfp-fade',
+            gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+            }
+        });
+    },    
+
+    
+    PogoApp.prototype.init = function() {
+        this.initStickyMenu();
+        this.initSmoothLink();
+        this.initMenuOpen();
+        this.initMenuClose();
+        this.initMenuToggleClose();
+        this.initFullPages();
+        this.initVideo();
+        this.initScreenshot();
+        this.initTestimonial();
+        this.initScreenMFP();
+    },
+    //init
+    $.PogoApp = new PogoApp, $.PogoApp.Constructor = PogoApp
+}(window.jQuery),
+
+//initializing
+function($) {
+    "use strict";
+    $.PogoApp.init();
+}(window.jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollDownIcon = document.getElementById('scroll-down-icon');
+
+  window.addEventListener('scroll', function() {
+    if (window.pageYOffset === 0) {
+      // Show the scroll-down icon on the first page
+      scrollDownIcon.style.opacity = '1';
+    } else {
+      // Hide the scroll-down icon on other pages
+      scrollDownIcon.style.opacity = '0';
+    }
+  });
+});
